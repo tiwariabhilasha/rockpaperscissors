3eefd7b289fe3e47249bfa8eca9302e98b4006a7
tiwariabhilasha <tiwariabhilasha7@gmail.com>
2017-01-21 18:21:15 +0000

    Update server.js

--- server.js
+++ server.js
@@ -8,16 +8,6 @@ app.get('/',function(req,res){
   //__dirname : It will resolve to your project folder.
 });
 
-app.get('/css/main.css',function(req,res){
-    res.sendFile(path.join(__dirname + '/css/main.css')); 
-});
-
-app.get('/js/main.js',function(req,res){
-    res.sendFile(path.join(__dirname + '/js/main.js')); 
-});
-
-
-
 app.listen(3000);
 
 console.log("Running at Port 3000");
